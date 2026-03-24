aedd00bd0a90d7d905535f93ba0bea5989bbbfed
Harry7986 <emarh.harry.code@gmail.com>
2026-03-24 03:15:29 +0100

    Progress Bar Demo 3

--- src/components/ProgressBarDemo.tsx
+++ src/components/ProgressBarDemo.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useInterval } from '../hooks';
+import { useInterval } from '../hooks/useInterval';
 
 export const ProgressBarDemo = () => {
   const [progress, setProgress] = useState(0);
